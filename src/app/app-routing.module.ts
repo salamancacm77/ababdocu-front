fd56cd735f3b1321f7e024e29c75a5cf309a5998
Esteban Mesa <roman.mesa@sofka.com.co>
2019-08-30 18:06:08 -0500

    Se corrigieron algunos formatos según TSLint

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import {ClassInfoComponent  } from "../app/pages/classes/class-info/class-info.component";
-import { MethodsInfoComponent } from "../app/pages/classes/methods-info/methods-info.component";
-import { TypesInfoComponent } from "../app/pages/classes/types-info/types-info.component";
-import { EventsInfoComponent } from "../app/pages/classes/events-info/events-info.component";
+import {ClassInfoComponent  } from '../app/pages/classes/class-info/class-info.component';
+import { MethodsInfoComponent } from '../app/pages/classes/methods-info/methods-info.component';
+import { TypesInfoComponent } from '../app/pages/classes/types-info/types-info.component';
+import { EventsInfoComponent } from '../app/pages/classes/events-info/events-info.component';
+import { FriendsInfoComponent } from './pages/classes/friends-info/friends-info.component';
 import { InheritanceComponent } from './pages/classes/inheritance/inheritance.component';
 
 const routes: Routes = [
@@ -22,6 +23,10 @@ const routes: Routes = [
     {
     path: 'classEvents',
     component: EventsInfoComponent
+  },
+  {
+    path: 'classFriends',
+    component: FriendsInfoComponent
   },
   {
     path: 'classInheritance',
--- src/app/pipes/date-format.pipe.ts
+++ src/app/pipes/date-format.pipe.ts
@@ -11,15 +11,14 @@ export class DateFormatPipe implements PipeTransform {
 
   transform(date: Date | string, format: string = 'yyyy-MM-dd'): string {
     if (date) {
-      let year = date.toString().substring(0,4);
-      let month = date.toString().substring(4,6);
-      let day = date.toString().substring(6,8);
-      date = new Date(parseInt(year),parseInt(month),parseInt(day)); 
+      const year = date.toString().substring(0, 4);
+      const month = date.toString().substring(4, 6);
+      const day = date.toString().substring(6, 8);
+      date = new Date( parseInt(year), parseInt(month), parseInt(day));
       date.setDate(date.getDate());
       return this.datePipe.transform(date, format);
-
     } else {
-      return 'ERROR'
+      return 'ERROR';
     }
   }
 
