c8576f6bb072595b8644f1097356ed9763a41860
Esteban Mesa <roman.mesa@sofka.com.co>
2019-09-04 09:51:55 -0500

    Cambié el nombre de la clase para probar friends class.

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -1,3 +1,4 @@
 export class AppConstants {
-    public static className: string = "ycl_tickets_manager_admin";
+    // public static className: string = "ycl_tickets_manager_admin";
+    public static className = 'zcl_docu_generator_emesa';
 }
